026e4c49f308e0722fbe41cb1feecc87b9072415
atrubaabid <atrubaabid7@gmail.com>
2026-05-18 20:54:29 +0500

    practicee

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './Header';
+import { blog } from './Data/blog';
+import weblogo from "./images/yc1.jpg"
+import { useState } from 'react';
 
 
 function App() {
+
+  // we write only js here
 
   let name = "Syeda Atruba";
   let l = [10, 20, 30, 40];
@@ -15,15 +20,66 @@ function App() {
   let status = true;
 
 
+
+  let Add = () => {
+    setcount(count + 1)
+
+  }
+
+  let [count, setcount] = useState(1)
+
+
+
+
+
+  let [pshow, setpshow] = useState(false)
+  let template = '';
+  if (pshow) {
+    template = <> <button onClick={() => setpshow(!pshow)}>Hide</button>
+      <p>WELCOME</p>
+    </>
+  } else {
+    template = <> <button onClick={() => setpshow(!pshow)}>show</button></>
+  }
+
+  let [show, setshow] = useState(false)
+
+
+
+
+
   return (
 
+    // we write html, css, js all here
     <div className='main'>
 
 
+      <button onClick={() => setshow(!show)}>{(show) ? "Hide" : "Show"}</button>
+      
+      {(show) ? <h1>Hello</h1> : ""}
 
-      {l.map((v) => {
+      {template}
+
+
+
+
+
+
+
+
+
+
+
+
+      {count}
+
+      <button onClick={() => Add()}>Add Data</button>
+
+
+
+      {l.map((v, i) => {
         return (
-          <p>{v}</p>
+          <p key={i}>{v}</p>
         )
       })}
 
@@ -34,7 +90,27 @@ function App() {
 
       {(status) ? <h1 style={{ color: "yellow", backgroundColor: "purple" }}>{name}</h1> : ""}
 
-      <Header></Header>
+      <Header email="atrubaabid@gmail.com" obj={obj}>
+        <h1 style={{ color: "red" }}>I'm Child of the header</h1>
+      </Header>
+
+      <Card></Card>
+      <Card></Card>
+      <Card></Card>
+      <Card></Card>
+
+      <img width={500} src={weblogo} /><br></br>
+      <img width={500} src={weblogo} />
+
+
+      {blog.map((v, i) => {
+        return (
+
+          <Productitems pitems={v} key={i} />
+        )
+      })}
+
+
 
     </div>
 
@@ -43,3 +119,23 @@ function App() {
 }
 
 export default App;
+
+// card
+
+function Card() {
+  return (
+    <h1>I'm Card</h1>
+  )
+}
+
+// Products
+
+function Productitems({ pitems }) {
+  return (
+    <div className='divy'>
+      <h4>{pitems.title}</h4>
+      <p>{pitems.body}</p>
+      <button>{pitems.id}</button>
+    </div>
+  )
+}
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import './Header.css'
 
-export default function Header() {
+export default function Header({obj, email, children}) {
+    console.log(children);
+    // let {obj} = props;
+    // console.log(obj.name);
+    // console.log(obj.class);
+    // console.log(obj.uni);
+
+    
   return (
-    <h1 style={{color:"green"}}>Header</h1>
+    <div className='hdiv'>
+    <h1 style={{color:"green"}}>Header {obj.class} {email} {children}</h1>
+    </div>
   )
 }
